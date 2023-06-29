9ca413d51eaf1047aa14aa303acb37f62fd9cdb6
losiik <49565548+losiik@users.noreply.github.com>
2023-06-30 01:44:29 +0400

    Update services.php

--- include/index/services.php
+++ include/index/services.php
@@ -3,15 +3,15 @@
         <div class="b-article ">
         <h2>Наши услуги</h2>
             <div class="b-article__gridContainer d-grid">
-                <a href="#outsourcingServers" class="b-service-btn"><div class="b-article__services_link">Аутсорсинг серверов</div></a>
-                <a href="#virtualisation" class="b-service-btn"><div class="b-article__services_link">Виртуализация</div></a>
-                <a href="#1CSupport" class="b-service-btn"><div class="b-article__services_link">Сопровождение программных продуктов 1С</div></a>
-                <a href="#PCSupport" class="b-service-btn"><div class="b-article__services_link">Обслуживание компьютеров</div></a>
-                <a href="#setUpNetwork" class="b-service-btn"><div class="b-article__services_link">Настройка сетевого оборудования</div></a>
-                <a href="#phoneSupport" class="b-service-btn"><div class="b-article__services_link">Обслуживание телефонии</div></a>
-                <a href="#businessAutomatisation" class="b-service-btn"><div class="b-article__services_link">Автоматизация бизнеса</div></a>
+                <a href="#outsourcingServers" class="b-article__services_link b-service-btn">Аутсорсинг серверов</a>
+               <a href="#virtualisation" class="b-article__services_link b-service-btn">Виртуализация</a>
+                <a href="#1CSupport" class=" b-article__services_link b-service-btn">Сопровождение программных продуктов 1С</a>
+                <a href="#PCSupport" class="b-article__services_link b-service-btn">Обслуживание компьютеров</a>
+                <a href="#setUpNetwork" class=" b-article__services_link b-service-btn">Настройка сетевого оборудования</a>
+                <a href="#phoneSupport" class="b-article__services_link b-service-btn">Обслуживание телефонии</a>
+                <a href="#businessAutomatisation" class="b-article__services_link b-service-btn">Автоматизация бизнеса</a>
                 <div class="secondary-primary-btn d-none d-lg-flex mt-2 align-items-center justify-content-center c-serviceOrder"><a href="#">ПОЗВОНИТЬ</a></div>
-                <a href="#video" class="b-service-btn"><div class="b-article__services_link">Видеонаблюдение</div></a>
+                <a href="#video" class="b-article__services_link b-service-btn">Видеонаблюдение</a>
                 <div class="secondary-primary-btn d-flex d-lg-none mt-2 align-items-center justify-content-center c-serviceOrder"><a href="#">ПОЗВОНИТЬ</a></div>
 
             </div>
